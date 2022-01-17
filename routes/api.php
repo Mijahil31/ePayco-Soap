795630e18b7a758c54b0aa9899a853f3ede82942
Mijahil Franchi <mijahil31@gmail.com>
2022-01-17 16:41:37 -0500

    Correcciones de las routes

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controller\ConfirmPayment;
-use App\Http\Controller\UserController;
-use App\Http\Controller\WalletController;
-use App\Http\Controller\PaymentController;
+use App\Http\Controllers\ConfirmPaymentController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\WalletController;
+use App\Http\Controllers\PaymentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,11 +18,14 @@ use App\Http\Controller\PaymentController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
-Route::resource('user', UserController::class);
-Route::resource('payment', PaymentController::class);
-Route::resource('wallet', WalletController::class);
-Route::resource('confirm', ConfirmPayment::class);
+
+Route::resources([
+    'user' => UserController::class,
+    'payment' => PaymentController::class,
+    'wallet' => WalletController::class,
+    'confirm' => ConfirmPaymentController::class,
+]);
